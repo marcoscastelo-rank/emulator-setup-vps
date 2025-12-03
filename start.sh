40adf0c62d7c2d5e4207e0b28f1fa44663b9319b
marcoscastelo-rank <marcos.castelo@rankmyapp.com.br>
2025-12-03 12:26:43 +0000

    fix: permissions

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export ANDROID_HOME="$HOME/Android/Sdk"
+export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
+export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
 
 ./android/start_emulator.sh
 sleep 30
